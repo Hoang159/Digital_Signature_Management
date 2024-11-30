773ede556adcc2b97c2c4974e867bd8b409fabe3
HoangLe <hoangbq73@gmail.com>
2024-11-30 11:04:06 +0700

    Chinh sua giao dien lan 1

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('TinhNangLink1').addEventListener('click', function() {
+    document.getElementById('TinhNangContent').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('TinhNangLink2').addEventListener('click', function() {
+    document.getElementById('TinhNangContent').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('LienHe').addEventListener('click', function() {
+    document.getElementById('LienHeContent').scrollIntoView({ behavior: 'smooth' });
+});
