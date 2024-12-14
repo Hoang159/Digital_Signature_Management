119a82cc66e8e4a2cf62f9e133ea870ea46e1b61
HoangLe <hoangbq73@gmail.com>
2024-12-15 06:01:38 +0700

    Cap nhat giao dien trang chu login logout

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+document.getElementById('GioiThieu').addEventListener('click', function() {
+    document.getElementById('GioiThieuContent').scrollIntoView({ behavior: 'smooth' });
+});
+
 document.getElementById('TinhNangLink1').addEventListener('click', function() {
     document.getElementById('TinhNangContent').scrollIntoView({ behavior: 'smooth' });
 });
@@ -6,6 +10,11 @@ document.getElementById('TinhNangLink2').addEventListener('click', function() {
     document.getElementById('TinhNangContent').scrollIntoView({ behavior: 'smooth' });
 });
 
+document.getElementById('HuongDan').addEventListener('click', function() {
+    document.getElementById('HuongDanContent').scrollIntoView({ behavior: 'smooth' });
+});
+
 document.getElementById('LienHe').addEventListener('click', function() {
     document.getElementById('LienHeContent').scrollIntoView({ behavior: 'smooth' });
 });
+
